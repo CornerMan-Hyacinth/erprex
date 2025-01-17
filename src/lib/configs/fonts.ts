91de371f0871b0708b46b2c5b68bcbf7f744fd37
Cornerstone Hyacinth <hycorner462@gmail.com>
2025-01-17 11:47:15 +0100

    Initial commit

---
+++ src/lib/configs/fonts.ts
@@ -0,0 +1,93 @@
+import localFont from "next/font/local";
+
+// DM Sans fonts
+export const dmSansRegular = localFont({
+  src: "./fonts/dmsans/DMSans-Regular.ttf",
+  variable: "--font-dmsans-regular",
+  weight: "100 900",
+});
+
+export const dmSansItalic = localFont({
+  src: "./fonts/dmsans/DMSans-Italic.ttf",
+  variable: "--font-dmsans-italic",
+  weight: "100 900",
+});
+
+export const dmSansMedium = localFont({
+  src: "./fonts/dmsans/DMSans-Medium.ttf",
+  variable: "--font-dmsans-medium",
+  weight: "100 900",
+});
+
+export const dmSansMediumItalic = localFont({
+  src: "./fonts/dmsans/DMSans-MediumItalic.ttf",
+  variable: "--font-dmsans-mediumitalic",
+  weight: "100 900",
+});
+
+export const dmSansSemiBold = localFont({
+  src: "./fonts/dmsans/DMSans-SemiBold.ttf",
+  variable: "--font-dmsans-semibold",
+  weight: "100 900",
+});
+
+export const dmSansBold = localFont({
+  src: "./fonts/dmsans/DMSans-Bold.ttf",
+  variable: "--font-dmsans-bold",
+  weight: "100 900",
+});
+
+export const dmSansBoldItalic = localFont({
+  src: "./fonts/dmsans/DMSans-BoldItalic.ttf",
+  variable: "--font-dmsans-bolditalic",
+  weight: "100 900",
+});
+
+// Inter fonts
+export const interRegular = localFont({
+  src: "./fonts/inter/Inter-Regular.ttf",
+  variable: "--font-inter-regular",
+  weight: "100 900",
+});
+
+export const interItalic = localFont({
+  src: "./fonts/inter/Inter-Italic.ttf",
+  variable: "--font-inter-italic",
+  weight: "100 900",
+});
+
+export const interLight = localFont({
+  src: "./fonts/inter/Inter-Light.ttf",
+  variable: "--font-inter-light",
+  weight: "100 900",
+});
+
+export const interMedium = localFont({
+  src: "./fonts/inter/Inter-Medium.ttf",
+  variable: "--font-inter-medium",
+  weight: "100 900",
+});
+
+export const interMediumItalic = localFont({
+  src: "./fonts/inter/Inter-MediumItalic.ttf",
+  variable: "--font-inter-mediumitalic",
+  weight: "100 900",
+});
+
+export const interSemiBold = localFont({
+  src: "./fonts/inter/Inter-SemiBold.ttf",
+  variable: "--font-inter-semibold",
+  weight: "100 900",
+});
+
+export const interBold = localFont({
+  src: "./fonts/inter/Inter-Bold.ttf",
+  variable: "--font-inter-bold",
+  weight: "100 900",
+});
+
+export const interBoldItalic = localFont({
+  src: "./fonts/inter/Inter-BoldItalic.ttf",
+  variable: "--font-inter-bolditalic",
+  weight: "100 900",
+});
